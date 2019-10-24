775587f0618ce53d898d75877d67d5783b2fd0a3
javad <ghaedi1993@gmail.com>
2019-10-25 02:36:20 +0330

    setting up routes and webpack historyapifallback
    we set up routes by defining a <browserroute> and a few <router> s inside a div . and configured the webpack file so whenever we ask for another route the request doesnt go anywhere but get backs to the / page but depending on the words after / asks for a component refrencing that path

--- src/app.js
+++ src/app.js
@@ -5,9 +5,26 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
+const ExpenseDashboardPage = ()=>(
+    <div>
+        This is from my dashboard component
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        This is from my add expense component
+    </div>
+);
+
+//we add exact={true} to return ExpenseDashboardPage exactly when / is not when /create of others get called 
+//if do not define exact={true} the component of path / will be loadded in every single path 
 const routes = (
     <BrowserRouter>
-    
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true} /> 
+            <Route path="/create" component={AddExpensePage} />
+        </div>    
     </BrowserRouter>
 );
 
