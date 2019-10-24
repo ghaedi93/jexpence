670a3df5b2f2c91ae13f50bccf1a4824d7bdeaf9
javad <ghaedi1993@gmail.com>
2019-10-25 02:53:57 +0330

    countueing with demo routes

--- src/app.js
+++ src/app.js
@@ -17,13 +17,27 @@ const AddExpensePage = () => (
     </div>
 );
 
+const EditExpencePage=()=>(
+    <div>
+        This is Edit page
+    </div>
+);
+
+const HelpPage=()=>(
+    <div>
+        This is Help page
+    </div>
+);
+
 //we add exact={true} to return ExpenseDashboardPage exactly when / is not when /create of others get called 
 //if do not define exact={true} the component of path / will be loadded in every single path 
 const routes = (
     <BrowserRouter>
         <div>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} /> 
-            <Route path="/create" component={AddExpensePage} />
+            <Route path="/"         component={ExpenseDashboardPage} exact={true} /> 
+            <Route path="/create"   component={AddExpensePage} />
+            <Route path="/edit"     component={EditExpencePage} />
+            <Route path='/help'     component={HelpPage}/>
         </div>    
     </BrowserRouter>
 );
