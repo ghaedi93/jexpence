8862d23be6fae0f0fd1d291a048af34c4c58c338
javad <ghaedi1993@gmail.com>
2019-10-25 00:51:55 +0330

    biolerPlate
    just started the work on a basic scafold

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React         from 'react';
+import ReactDOM      from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+ReactDOM.render(<p>The test</p>, document.getElementById('app'));
