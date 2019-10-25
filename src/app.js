89008152e278c074731625a53302954560cf0480
javad <ghaedi1993@gmail.com>
2019-10-25 15:43:47 +0330

    setting routers in app.js file
    we set some routes in our app and tweak afew of its properties

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React         from 'react';
 import ReactDOM      from 'react-dom';
 //BrowserRouter is to create router once and router is for every page 
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
+
+//to use activeClassName > isActive properly we should you exact={true} otherwise the home icon get the same class as 
+//the route we are in it because they both start with a / 
+const Header = ()=>(
+    <div>
+        <h1>Jexpense</h1>
+       
+            <NavLink to="/"       activeClassName="isActive" exact={true}>Home   </NavLink>
+            <NavLink to="/create" activeClassName="isActive" exact={true}>Create </NavLink>
+            <NavLink to="/edit"   activeClassName="isActive" exact={true}>Edit   </NavLink>
+            <NavLink to="/help"   activeClassName="isActive" exact={true}>Help   </NavLink>
+
+    </div>
+);
 
 const ExpenseDashboardPage = ()=>(
     <div>
@@ -28,17 +42,30 @@ const HelpPage=()=>(
         This is Help page
     </div>
 );
+const NotFoundPage=()=>(
+    <div>
+        404 -  <Link to="/">Go Home</Link>
+    </div>
+);
 
 //we add exact={true} to return ExpenseDashboardPage exactly when / is not when /create of others get called 
 //if do not define exact={true} the component of path / will be loadded in every single path 
+//in order to make use of 404 pages we have to import switch 
+//switch go through route maps and stops the moment it get the first match so it does'nt get afterward matchs
+//BrowserRouter should only have one child so we have to put a div in it 
 const routes = (
     <BrowserRouter>
-        <div>
+    <div>
+        <Header/>
+        <Switch>
             <Route path="/"         component={ExpenseDashboardPage} exact={true} /> 
             <Route path="/create"   component={AddExpensePage} />
             <Route path="/edit"     component={EditExpencePage} />
             <Route path='/help'     component={HelpPage}/>
-        </div>    
+            <Route         component={NotFoundPage}/>
+        </Switch>  
+    </div>
+         
     </BrowserRouter>
 );
 
